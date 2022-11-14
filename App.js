d5aef2990ac63e00342d91b3d797143cb43e111f
LucasTMB <lucas.tmb708@gmail.com>
2022-11-14 19:09:22 -0300

    First commit

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+import foto from './assets/Foto-Perfil.jpg';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Lucas Teixeira</Text>
+      <Image source={foto}></Image>
       <StatusBar style="auto" />
     </View>
   );
